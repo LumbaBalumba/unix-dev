6a986fe9f3e2b666ee89bb2e3ff7cac8b903ee9d
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-10-01 20:54:01 +0300

    .

--- 02_Multifile/scripts/test.sh
+++ 02_Multifile/scripts/test.sh
@@ -2,7 +2,8 @@
 
 readonly BUILD_DIR=./bin
 readonly BINFILES=(prog prog-a prog-so)
+readonly TESTDIR=./test
 
 for binfile in ${BINFILES[@]}; do
-	LD_LIBRARY_PATH=./bin $BUILD_DIR/$binfile
+	LD_LIBRARY_PATH=./bin $BUILD_DIR/$binfile >$TESTDIR/$binfile
 done
