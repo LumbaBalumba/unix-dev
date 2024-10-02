f34cb0f0f2d246a69f066ccef1fd3670f237eea8
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-10-02 10:45:07 +0300

    add tests

--- 02_Multifile/scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-readonly BUILD_DIR=./bin
-readonly BINFILES=(prog prog-a prog-so)
-readonly TESTDIR=./test
-
-for binfile in ${BINFILES[@]}; do
-	LD_LIBRARY_PATH=./bin $BUILD_DIR/$binfile >$TESTDIR/$binfile
-done
